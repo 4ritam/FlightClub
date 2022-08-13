919a7b59e5cb08e9321e433bf4c66759bf9ace45
Pritam Das <daspritam4231@gmail.com>
2022-08-13 19:28:01 +0530

    flight data fetched

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,8 @@
+import requests
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    sheety_api = "https://api.sheety.co/16276d59486a8c0fd7fc97afa9e6760a/flightDeals/prices"
+    price_list = requests.get(sheety_api).json()["prices"]
+    print(price_list)
